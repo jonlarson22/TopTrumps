bd198e58eeaf7444df1c93e5d4f29a66ac44d6b0
Jonathan <40722881+jonlarson22@users.noreply.github.com>
2026-09-04 17:00:25 -0600

    Add initial cardDeck with dragon stats

---
+++ deck.js
@@ -0,0 +1,32 @@
+export const cardDeck = [
+  { 
+    id: 'night_fury', 
+    name: 'Night Fury', 
+    stats: { firepower: 14, speed: 20, armor: 18, venom: 0 } 
+  },
+  { 
+    id: 'monstrous_nightmare', 
+    name: 'Monstrous Nightmare', 
+    stats: { firepower: 15, speed: 16, armor: 12, venom: 0 } 
+  },
+  { 
+    id: 'deadly_nadder', 
+    name: 'Deadly Nadder', 
+    stats: { firepower: 18, speed: 8, armor: 16, venom: 16 } 
+  },
+  { 
+    id: 'gronckle', 
+    name: 'Gronckle', 
+    stats: { firepower: 14, speed: 4, armor: 20, venom: 0 } 
+  },
+  { 
+    id: 'hideous_zippleback', 
+    name: 'Hideous Zippleback', 
+    stats: { firepower: 14, speed: 10, armor: 10, venom: 14 } 
+  },
+  { 
+    id: 'terrible_terror', 
+    name: 'Terrible Terror', 
+    stats: { firepower: 8, speed: 10, armor: 6, venom: 12 } 
+  }
+];
